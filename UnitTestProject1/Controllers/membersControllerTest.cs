452089d246049cdfad37350cdbde7865874d7348
michaelkruit <michael_mikin@hotmail.com>
2017-06-17 20:27:02 +0200

    Added simple unit test

---
+++ UnitTestProject1/Controllers/membersControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeltaImpuls2.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTestProject1.Controllers
+{
+    [TestClass]
+    public class membersControllerTest
+    {
+        membersController controller = new membersController();
+        [TestMethod]
+        public void members_Index()
+        {
+            // Act
+            ViewResult result = controller.Index("", null, null, null) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void members_Create()
+        {
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+            
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void members_Edit()
+        {
+            // Act
+            ViewResult result = controller.Edit(1) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void members_Delete()
+        {
+            // Act
+            ViewResult result = controller.Delete(2) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void members_DeleteConfirmed()
+        {
+            // Act
+            ViewResult result = controller.DeleteConfirmed(2) as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
