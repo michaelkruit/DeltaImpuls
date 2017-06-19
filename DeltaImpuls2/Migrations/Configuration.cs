3dee28a89e5acb7fe2cf9953acf0d442702cace3
michaelkruit <michael_mikin@hotmail.com>
2017-06-19 10:30:59 +0200

    Added user seed

--- DeltaImpuls2/Migrations/Configuration.cs
+++ DeltaImpuls2/Migrations/Configuration.cs
@@ -1,32 +1,30 @@
 namespace DeltaImpuls2.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DeltaImpuls2.DAL.DeltaImpulsContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DeltaImpuls2.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            ContextKey = "DeltaImpuls2.DAL.DeltaImpulsContext";
+            ContextKey = "DeltaImpuls2.DAL.DeltaImpulsContext.cs";
         }
 
-        protected override void Seed(DeltaImpuls2.DAL.DeltaImpulsContext context)
+        protected override void Seed(DeltaImpuls2.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var passwordHash = new PasswordHasher();
+            string password = passwordHash.HashPassword("@Test123");
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "Deltaimpuls@hotmail.com",
+                    PasswordHash = password
+                });
         }
     }
 }
